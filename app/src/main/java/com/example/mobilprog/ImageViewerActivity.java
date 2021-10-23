42b86bd243d8a94277269d968cdc9929aa13f294
Bognet-projest <bognar.balazs@bognet.hu>
2021-10-23 17:05:25 +0200

    Added markers and onClick listeners

--- app/src/main/java/com/example/mobilprog/ImageViewerActivity.java
+++ app/src/main/java/com/example/mobilprog/ImageViewerActivity.java
@@ -12,9 +12,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
@@ -55,6 +58,35 @@ public class ImageViewerActivity extends AppCompatActivity {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Toast.makeText(ImageViewerActivity.this, "Delete failed!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+        ;
+    }
+
+    public void onOpen(View view) {
+        image.getMetadata()
+                .addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
+                    @Override
+                    public void onSuccess(StorageMetadata storageMetadata) {
+                        String latStr = storageMetadata.getCustomMetadata("Latitude");
+                        String lngStr = storageMetadata.getCustomMetadata("Longitude");
+
+                        assert latStr != null;
+                        double lat = Double.parseDouble(latStr);
+                        assert lngStr != null;
+                        double lng = Double.parseDouble(lngStr);
+
+
+                        Intent i = new Intent(ImageViewerActivity.this, MapsActivity.class);
+                        i.putExtra("lat", lat);
+                        i.putExtra("lng", lng);
+                        startActivity(i);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ImageViewerActivity.this, "Unable to open!", Toast.LENGTH_SHORT).show();
                     }
                 })
         ;
--- app/src/main/java/com/example/mobilprog/MapsActivity.java
+++ app/src/main/java/com/example/mobilprog/MapsActivity.java
@@ -5,16 +5,27 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.example.mobilprog.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
 
@@ -32,7 +43,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
-
     }
 
     @Override
@@ -44,7 +54,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         mMap.setMyLocationEnabled(true);
-
+        loadLocations();
+        mMap.setOnMarkerClickListener(this);
+        focusOnLocation();
     }
 
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        StorageReference imageRef = (StorageReference) marker.getTag();
+        Intent i = new Intent(this, ImageViewerActivity.class);
+
+        assert imageRef != null;
+        i.putExtra("image", imageRef.getName());
+        startActivity(i);
+
+        return false;
+    }
+
+    private void loadLocations() {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference listRef = storage.getReference().child("Images/");
+
+        listRef.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        for (StorageReference item : listResult.getItems()) {
+                            item.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
+                                @Override
+                                public void onSuccess(StorageMetadata storageMetadata) {
+                                    String latStr = storageMetadata.getCustomMetadata("Latitude");
+                                    String lngStr = storageMetadata.getCustomMetadata("Longitude");
+
+                                    assert latStr != null;
+                                    double lat = Double.parseDouble(latStr);
+                                    assert lngStr != null;
+                                    double lng = Double.parseDouble(lngStr);
+
+                                    LatLng position = new LatLng(lat, lng);
+                                    mMap.addMarker(new MarkerOptions().position(position))
+                                    .setTag(item);
+                                }
+                            });
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MapsActivity.this, "Data load error!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void focusOnLocation() {
+        double lat, lng;
+        lat = getIntent().getDoubleExtra("lat", 0);
+        lng = getIntent().getDoubleExtra("lng", 0);
+        if (lat != 0 && lng != 0) {
+            LatLng location = new LatLng(lat, lng);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+        }
+    }
 }
